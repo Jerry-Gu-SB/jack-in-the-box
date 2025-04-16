5aba7dd64da56ea31bd5857d53d68533ec589a5e
josephchoe <choejoseph26@gmail.com>
2025-04-16 12:18:41 -0400

    implemented angled jump using arrow keys

--- jack-in-the-box/Assets/Scripts/PlayerMovement.cs
+++ jack-in-the-box/Assets/Scripts/PlayerMovement.cs
@@ -8,10 +8,12 @@ public class PlayerMovement : MonoBehaviour
     public float maxJump;
     public float minJump;
     public float jumpMultiplier;
+    public float angleMultiplier;
     
     // charged jump taken from https://discussions.unity.com/t/long-press-for-charged-jump/202543
     private float charger;
     private Boolean discharge;
+    private float angle;
 
     private Rigidbody2D rb;
     
@@ -22,11 +24,23 @@ public class PlayerMovement : MonoBehaviour
         maxJump = 20f;
         minJump = 10f;
         jumpMultiplier = 30f;
+        angleMultiplier = 5f;
     }
 
     
     void Update()
-    {
+    {   
+        if(Input.GetKey(KeyCode.LeftArrow)) 
+        {
+            angle -= Time.deltaTime * angleMultiplier;
+            angle = Math.Max(angle, -20f);
+        }
+        if(Input.GetKey(KeyCode.RightArrow)) 
+        {
+            angle += Time.deltaTime * angleMultiplier;
+            angle = Math.Min(angle, 20f);
+        }
+
         if (Input.GetKey(KeyCode.Space))
         {
             charger += Time.deltaTime;
@@ -41,9 +55,10 @@ public class PlayerMovement : MonoBehaviour
     {
         if (discharge)
         {
-            rb.velocity = new Vector2(rb.velocity.x, Math.Min(minJump + jumpMultiplier * charger, maxJump));
+            rb.velocity = new Vector2(rb.velocity.x+angle, Math.Min(minJump + jumpMultiplier * charger, maxJump));
             discharge = false;
             charger = 0f;
+            angle = 0;
         }
     }
 
