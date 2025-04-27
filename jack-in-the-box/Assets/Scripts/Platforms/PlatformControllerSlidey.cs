f973d06b24849c4993e52c4cba16f25b3925f760
Jerry Gu <ncq9fn@virginia.edu>
2025-04-26 21:50:15 -0400

    added sliding sound effects

---
+++ jack-in-the-box/Assets/Scripts/Platforms/PlatformControllerSlidey.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+namespace Platforms
+{
+    public class PlatformControllerSlidey : MonoBehaviour
+    {
+        [SerializeField]
+        private PhysicsMaterial2D physicsMaterial2D;
+
+        public AudioSource slidingAudioSource;
+
+        public float platformFriction = 0.7f;
+        public float platformBounciness = 0f;
+
+        // Start is called before the first frame update
+        private void Start()
+        {
+            physicsMaterial2D.friction = platformFriction;
+            physicsMaterial2D.bounciness = platformBounciness;
+
+            var col = GetComponent<Collider2D>();
+            if (col == null) return;
+            col.sharedMaterial = null; // force refresh to load custom values
+            col.sharedMaterial = physicsMaterial2D;
+        }
+        
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.collider.CompareTag("Player"))
+            {
+                slidingAudioSource.Play();
+            }
+        }
+        
+        private void OnCollisionExit2D(Collision2D collision)
+        {
+            if (collision.collider.CompareTag("Player"))
+            {
+                slidingAudioSource.Stop();
+            }
+        }
+    }
+}
